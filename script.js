d0030b1add489e1289db61884fbc4c570caf0630
Pelatihan_TIK_4ndik4WC <ragel8642@gmail.com>
2025-10-07 12:45:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function(){
-  alert('Selamat datang di web yang saya buat, HAVE A NICE DAY');
+  alert('Welcome to my Program, HAVE A NICE DAY');
 });
